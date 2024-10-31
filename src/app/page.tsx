2d2cb72dbaafda87d324abd10bf0bef4eabfac59
muneebhashmap <smuneebh13@gmail.com>
2024-10-31 14:11:46 -0400

    fixed loading and thread date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,36 @@
 "use client";
 
 import { useEffect, useMemo } from "react";
-import { UserButton } from "../features/auth/components/user-button";
 
 import { useGetWorkspaces } from "../features/workspaces/api/use-get-workspaces";
 import { useCreateWorkspaceModal } from "../features/workspaces/store/use-create-workspace-modal";
 import { useRouter } from "next/navigation";
+import { Loader } from "lucide-react";
 
 export default function Home() {
   const router = useRouter();
 
-  const[open, setOpen] = useCreateWorkspaceModal();
-  
-  const {data, isLoading} = useGetWorkspaces();
-   //picking the first in the array, also being careful as it can be undefined
-  const workspaceId = useMemo(() => data?.[0]?._id, [data]); 
+  const [open, setOpen] = useCreateWorkspaceModal();
 
-  useEffect(()=> {
+  const { data, isLoading } = useGetWorkspaces();
+  //picking the first in the array, also being careful as it can be undefined
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
     if (isLoading) return;
 
-    if(workspaceId) {
-      router.replace(`/workspace/${workspaceId}`)
+    if (workspaceId) {
+      router.replace(`/workspace/${workspaceId}`);
       console.log("Redirect to workspace");
-    } else if (!open) { // makes the user forced to open a workspace, if there are no workspaces, u can't even close dialog (setopen is true)
+    } else if (!open) {
+      // makes the user forced to open a workspace, if there are no workspaces, u can't even close dialog (setopen is true)
       setOpen(true);
     }
   }, [workspaceId, isLoading, open, setOpen, router]);
 
   return (
-    <div>
-      <UserButton/>
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground"></Loader>
     </div>
   );
 }
